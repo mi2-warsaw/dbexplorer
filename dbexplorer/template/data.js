56200ea3cdb7415ff87053ff554d02db805c2287
Karol Prusinowski <prusinowskik@student.mini.pw.edu.pl>
2018-06-10 14:15:23 +0200

    'Distinct' column null to empty string

--- dbexplorer/template/data.js
+++ dbexplorer/template/data.js
@@ -3,21 +3,26 @@
  */
 class Data {
 	constructor() {
-		return this.clearEmptyTables({{data}});
+		return this.modifyTables({{data}});
 	}
 	
 	/**
-	 * Delete unnecessary data from columns on empty tables. It leaves the name of the column and the SQL type
+	 * Delete unnecessary data from columns on empty tables. It leaves the name of the column and the SQL type. Changes 'distinct' column to empty string if is null
 	 * @param {Data} data - raport data in json.
 	 * @return {Data} Cleared data.
 	 */
-	clearEmptyTables(data) {
+	modifyTables(data) {
 		data.tables.forEach(table => {
-			if(table.records == 0) {
-				table.columns.forEach(column => {
+			table.columns.forEach(column => {
+				if(table.records == 0) {
 					column.data = column.data.filter(data => data.key === 'Name' || data.key == 'SQL Type')
-				});
-			}
+				}
+				column.data.forEach(data => {
+					if (data.key === 'Distinct' && data.value === null) {
+						data.value = '';
+					}
+				})
+			});
 		});
 		return data;
 	}
